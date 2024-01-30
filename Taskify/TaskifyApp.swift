cade222a0dfaa9ce3c1435f7523160f6d521e264
Jan Zimola <zimolajan798@gmail.com>
2024-01-30 07:02:42 +0100

    Initial Commit

---
+++ Taskify/TaskifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskifyApp.swift
+//  Taskify
+//
+//  Created by Jan Zimola on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
